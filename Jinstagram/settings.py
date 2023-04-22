5f02aad83a50b5a6ab853eb28ea0397a9deee661
mychew__ <tkdlek11112@gmail.com>
2023-04-23 00:58:24 +0900

    :art: 테스트 코드 추가

--- Jinstagram/settings.py
+++ Jinstagram/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import sys
 from pathlib import Path
 import os
 
@@ -78,12 +78,6 @@ WSGI_APPLICATION = 'Jinstagram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
@@ -96,6 +90,7 @@ DATABASES = {
         'OPTIONS': {'charset': 'utf8mb4'},
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from django.contrib.auth.hashers import make_password
+
+from user.models import User
+
 
 # Create your tests here.
+class TestUser(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create(email="login_test@gmail.com",
+                            nickname="login_test",
+                            name="login_test",
+                            password=make_password("login_test"),
+                            profile_image="default_profile.png"
+                            )
+
+    def test_1(self):
+        self.assertEqual(1, 1)
+
+    def test_post_join(self):
+        response = self.client.post('/user/join',
+                                    dict(
+                                        email="test_email@google.com",
+                                        nickname="test_nickname",
+                                        name="test_name",
+                                        password="test_password")
+                                    )
+        self.assertEqual(response.status_code, 200)
+
+        test_user = User.objects.filter(email="test_email@google.com").first()
+        self.assertEqual(test_user.nickname, "test_nickname")
+        self.assertEqual(test_user.name, "test_name")
+        self.assertTrue(test_user.check_password("test_password"))
+
+    def test_post_login(self):
+        response = self.client.post('/user/login',
+                                    dict(
+                                        email="login_test@gmail.com",
+                                        password="login_test")
+                                    )
+        self.assertEqual(response.status_code, 200)
